f3fb4adfea6dcba02593ba989aaa6cc0aad3575e
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-02-11 17:08:28 +0200

    added passport js draft

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} = process.env;
 
 const app = express();
 
+// draft
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get('/', (req, res) => {
-  console.log('Got a get request.');
+
   res.send({message: 'Hello buddy!'})
 });
 
